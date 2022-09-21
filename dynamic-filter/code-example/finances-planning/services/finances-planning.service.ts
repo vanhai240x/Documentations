ab99d058d355260231aa19ac2e6218d448325873
Tran Van Hai <haitv@danhkhoi.com.vn>
2022-09-21 14:17:54 +0700

    update codde-example

--- dynamic-filter/code-example/finances-planning/services/finances-planning.service.ts
+++ dynamic-filter/code-example/finances-planning/services/finances-planning.service.ts
@@ -1,6 +1,26 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+export interface IDynamicFilter {
+  pagination: {
+    pageSize: number;
+    pageNumber: number;
+    isPaging: boolean;
+  };
+  groupFilters: {
+    filters: {
+      operator: 'eq' | 'neq' | 'gt' | 'gte' | 'lt' | 'lte' | 'contains' | 'fc';
+      field: string;
+      value: string;
+    }[];
+    logic: { value: 'or' | 'and' };
+  }[];
+  sort: {
+    predicate: string;
+    reverse: boolean;
+  }[];
+}
+
 @Injectable({
   providedIn: 'root',
 })
@@ -10,7 +30,7 @@ export class FinancesPlanningService {
   ) {}
 
   getFinancesPlanning(paging?: any, groupFilters?: any, sort?: any) {
-    let body = {
+    let body: IDynamicFilter = {
       pagination: paging,
       groupFilters: groupFilters,
       sort: sort,
